d670f9f79510f310e3aafc14a6fe2a657b7e85b2
Aaro Koinsaari <aaro.koinsaari@protonmail.ch>
2026-02-20 21:33:58 +0200

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val ktor_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("io.ktor.plugin") version "3.4.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
+}
+
+group = "com.aarokoinsaari"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-host-common")
+    implementation("io.ktor:ktor-server-status-pages")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("org.apache.commons:commons-compress:1.28.0")
+    implementation("org.eclipse.angus:jakarta.mail:2.0.5")
+    implementation("io.lettuce:lettuce-core:7.4.0.RELEASE")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "email-ingestion-service"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
